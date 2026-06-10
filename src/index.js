e08b7a8a219740989c7b76e129f9b42be6e076bd
freepentests <252639873+freepentests@users.noreply.github.com>
2026-06-10 12:31:27 +0100

    added strings

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default class ByteBuffer {
 		this.littleEndian = littleEndian ?? ByteBuffer.DEFAULT_ENDIAN;
 
 		this.buffer = new ArrayBuffer(capacity);
-		this.view = new DataView(buffer);
+		this.view = new DataView(this.buffer);
 
 		this.offset = 0;
 		this.markedOffset = -1;
@@ -46,73 +46,85 @@ export default class ByteBuffer {
 	writeInt16(number) {
 		this.view.setInt16(this.offset, number, this.littleEndian);
 		this.offset += 2;
+
 		return this;
 	}
 
 	writeUint16(number) {
 		this.view.setUint16(this.offset, number, this.littleEndian);
 		this.offset += 2;
+
 		return this;
 	}
 
 	readInt16() {
 		const number = this.view.getInt16(this.offset++, this.littleEndian);
 		this.offset += 2;
+
 		return number;
 	}
 
 	readUint16() {
 		const number = this.view.getUint16(this.offset, this.littleEndian);
 		this.offset += 2;
+
 		return number;
 	}
 
 	writeInt32(number) {
 		this.view.setInt32(this.offset, number, this.littleEndian);
 		this.offset += 4;
+
 		return this;
 	}
 
 	writeUint32(number) {
 		this.view.setUint32(this.offset, number, this.littleEndian);
 		this.offset += 4;
+
 		return this;
 	}
 
 	readInt32() {
 		const number = this.view.getInt32(this.offset++, this.littleEndian);
 		this.offset += 4;
+
 		return number;
 	}
 
 	readUint32() {
 		const number = this.view.getUint32(this.offset, this.littleEndian);
 		this.offset += 4;
+
 		return number;
 	}
 
-	writeInt64(number) {
-		this.view.setInt32(this.offset, number, this.littleEndian);
-		this.offset += 8;
+	static calculateUTF8Bytes(string) {
+		const stringContents = new TextEncoder().encode(string);
+		return stringContents.length;
+	}
+
+	writeIString(string) {
+		this.writeUint32(string.length);
+		this.writeUTF8String(string);
+
 		return this;
 	}
 
-	writeUint64(number) {
-		this.view.setUint32(this.offset, number, this.littleEndian);
-		this.offset += 8;
+	writeCString(string) {
+		this.writeUTF8String(string);
+		this.writeUint8(0); // null terminator
+
 		return this;
 	}
 
-	readInt64() {
-		const number = this.view.getInt32(this.offset++, this.littleEndian);
-		this.offset += 8;
-		return number;
-	}
+	writeUTF8String(string) {
+		const stringContents = new TextEncoder().encode(string);
+		stringContents.forEach(charCode => {
+			this.writeUint8(charCode);
+		});
 
-	readUint64() {
-		const number = this.view.getUint32(this.offset, this.littleEndian);
-		this.offset += 8;
-		return number;
+		return this;
 	}
 }
 
@@ -122,5 +134,4 @@ ByteBuffer.prototype.writeUInt8 = ByteBuffer.prototype.writeUint8;
 ByteBuffer.prototype.readUInt8 = ByteBuffer.prototype.readUint8;
 ByteBuffer.prototype.writeByte = ByteBuffer.prototype.writeUint8;
 ByteBuffer.prototype.readByte = ByteBuffer.prototype.readUint8;
-
 
