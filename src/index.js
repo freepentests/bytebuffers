232c648b6161ad3bbed22bfd3e101642a94846d4
freepentests <252639873+freepentests@users.noreply.github.com>
2026-06-10 10:53:22 +0100

    lolololol

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import './Modules/Types/Ints/Uint8.js';
-//import './Modules/Types/Ints/Uint16.js';
-//import './Modules/Types/Ints/Uint32.js';
-//import './Modules/Types/Ints/Uint64.js';
-
 export default class ByteBuffer {
 	#capacity;
 
@@ -21,13 +16,111 @@ export default class ByteBuffer {
 		this.limit = capacity ?? ByteBuffer.DEFAULT_CAPACITY;
 		this.littleEndian = littleEndian ?? ByteBuffer.DEFAULT_ENDIAN;
 
-		this.buffer = Buffer.alloc(capacity);
+		this.buffer = new ArrayBuffer(capacity);
 		this.view = new DataView(buffer);
 
 		this.offset = 0;
 		this.markedOffset = -1;
 	}
+
+	writeInt8(number) {
+		this.view.setInt8(this.offset++, number, this.littleEndian);
+		return this;
+	}
+
+	writeUint8(number) {
+		this.view.setUint8(this.offset++, number, this.littleEndian);
+		return this;
+	}
+
+	readUint8() {
+		const number = this.view.getUint8(this.offset++, this.littleEndian);
+		return number;
+	}
+
+	readInt8() {
+		const number = this.view.getInt8(this.offset++, this.littleEndian);
+		return number;
+	}
+
+	writeInt16(number) {
+		this.view.setInt16(this.offset, number, this.littleEndian);
+		this.offset += 2;
+		return this;
+	}
+
+	writeUint16(number) {
+		this.view.setUint16(this.offset, number, this.littleEndian);
+		this.offset += 2;
+		return this;
+	}
+
+	readInt16() {
+		const number = this.view.getInt16(this.offset++, this.littleEndian);
+		this.offset += 2;
+		return number;
+	}
+
+	readUint16() {
+		const number = this.view.getUint16(this.offset, this.littleEndian);
+		this.offset += 2;
+		return number;
+	}
+
+	writeInt32(number) {
+		this.view.setInt32(this.offset, number, this.littleEndian);
+		this.offset += 4;
+		return this;
+	}
+
+	writeUint32(number) {
+		this.view.setUint32(this.offset, number, this.littleEndian);
+		this.offset += 4;
+		return this;
+	}
+
+	readInt32() {
+		const number = this.view.getInt32(this.offset++, this.littleEndian);
+		this.offset += 4;
+		return number;
+	}
+
+	readUint32() {
+		const number = this.view.getUint32(this.offset, this.littleEndian);
+		this.offset += 4;
+		return number;
+	}
+
+	writeInt64(number) {
+		this.view.setInt32(this.offset, number, this.littleEndian);
+		this.offset += 8;
+		return this;
+	}
+
+	writeUint64(number) {
+		this.view.setUint32(this.offset, number, this.littleEndian);
+		this.offset += 8;
+		return this;
+	}
+
+	readInt64() {
+		const number = this.view.getInt32(this.offset++, this.littleEndian);
+		this.offset += 8;
+		return number;
+	}
+
+	readUint64() {
+		const number = this.view.getUint32(this.offset, this.littleEndian);
+		this.offset += 8;
+		return number;
+	}
 }
 
 ByteBuffer.prototype.__isByteBuffer__ = true;
+
+ByteBuffer.prototype.writeUInt8 = ByteBuffer.prototype.writeUint8;
+ByteBuffer.prototype.readUInt8 = ByteBuffer.prototype.readUint8;
+ByteBuffer.prototype.writeByte = ByteBuffer.prototype.writeUint8;
+ByteBuffer.prototype.readByte = ByteBuffer.prototype.readUint8;
+
 
